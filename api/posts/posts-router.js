c6e48bab797773cc6a1fe0f6283ade8717709bf4
AytacSahin <haytacsahin@gmail.com>
2023-06-13 16:38:22 +0300

    router initial get method done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,3 +1,15 @@
 // posts için gerekli routerları buraya yazın
 
 const router = require("express").Router()
+const postsModel = require("./posts-model");
+
+router.get("/", async (req, res) => {
+    try {
+        const all = await postsModel.find();
+        res.json(all);
+    } catch (error) {
+        res.status(500).json({ message: "Gönderiler alınamadı" });
+    }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
-// server için gerekli olanları burada ayarlayın
+// 🟢 server için gerekli olanları burada ayarlayın
 const express = require("express");
 const server = express();
 
-// posts router'ını buraya require edin ve bağlayın
-const postRouter = require("./posts/posts-router");
-
-// server'ın istekleri json formatında desteklemesi için; 
+// 🟢 server'ın istekleri json formatında desteklemesi için; 
 server.use(express.json());
 
-// server'ı dışarı çıkarıyoruz
+// 🟢 posts router'ını buraya require edin ve bağlayın
+const postRouter = require("./posts/posts-router");
+server.use("/api/posts", postRouter);
+
+// 🟢 server'ı dışarı çıkarıyoruz
 module.exports = server;
