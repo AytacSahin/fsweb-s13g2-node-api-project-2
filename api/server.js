9bb5344dc85d23dd541cf40283b5a80536cc466a
AytacSahin <haytacsahin@gmail.com>
2023-06-13 14:53:44 +0300

    start listening port 5000

--- api/server.js
+++ api/server.js
@@ -1,3 +1,8 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+
+const express = require("express");
+const server = express();
+
+module.exports = server;
